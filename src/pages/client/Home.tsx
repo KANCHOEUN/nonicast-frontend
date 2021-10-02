6ae703aeea6deed0c5748ed508badfc30688be33
KANCHOEUN <kancho1216@naver.com>
2021-10-02 06:56:04 +0000

    ✨ Impl filter by category logic

--- src/pages/client/Home.tsx
+++ src/pages/client/Home.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@apollo/client";
 import { CursorClickIcon } from "@heroicons/react/solid";
+import { RefreshIcon } from "@heroicons/react/outline";
 import gql from "graphql-tag";
-import React from "react";
+import React, { MouseEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import { PodcastItem } from "../../components/PodcastItem";
 import { bannerImgs } from "../../constants";
@@ -9,6 +10,7 @@ import {
   GetPodcastsQuery,
   GetPodcastsQuery_getPodcasts_podcasts,
 } from "../../__generated__/GetPodcastsQuery";
+import { PodcastCategory } from "../../__generated__/globalTypes";
 
 const GET_PODCASTS_QUERY = gql`
   query GetPodcastsQuery {
@@ -27,10 +29,15 @@ const GET_PODCASTS_QUERY = gql`
 `;
 
 export const Home: React.FC = () => {
+  const [select, setSelect] = useState("");
   const { data, loading, error } =
     useQuery<GetPodcastsQuery>(GET_PODCASTS_QUERY);
   let randomElem: GetPodcastsQuery_getPodcasts_podcasts | null;
   const bannerImg = bannerImgs[Math.floor(Math.random() * bannerImgs.length)];
+
+  const handleClickCategory = (e: MouseEvent<HTMLDivElement>) => {
+    setSelect((e.target as HTMLElement).innerText);
+  };
 
   if (!data || loading || error) {
     <div className="h-screen flex justify-center items-center">
@@ -58,7 +65,7 @@ export const Home: React.FC = () => {
             <span className="text-black text-xl flex flex-col sm:flex-row">
               Let's go to listen&nbsp;
               <Link
-                to={`/${randomElem?.id}`}
+                to={`/podcast/${randomElem?.id}`}
                 className="flex font-semibold hover:text-green-400 mt-2 sm:mt-0"
               >
                 " {randomElem?.title} "&nbsp;&nbsp;
@@ -69,12 +76,35 @@ export const Home: React.FC = () => {
         </div>
       </div>
 
-      {/* TODO: Category Filter */}
+      {/* Category Filter */}
+      <div className="px-6 w-full max-w-screen-sm sm:max-w-screen-xl sm:mx-auto flex justify-content items-center">
+        <div className="flex flex-wrap rounded-full">
+          {Object.values(PodcastCategory).map((category, idx) => (
+            <div
+              key={idx}
+              data-category={category}
+              className={`max-x-max rounded-full shadow cursor-pointer px-3 py-2 mr-2 mb-2 hover:bg-gray-100 transition duration-500 ${
+                category === select
+                  ? "text-white bg-yellow-400 hover:bg-yellow-400"
+                  : ""
+              }`}
+              onClick={handleClickCategory}
+            >
+              {category}
+            </div>
+          ))}
+          <RefreshIcon
+            className="w-6 cursor-pointer text-black text-opacity-30 hover:text-opacity-50 transition duration-500"
+            onClick={() => setSelect("")}
+          />
+        </div>
+      </div>
 
       <div className="mt-7 max-w-screen-xl mx-auto">
         {data &&
-          data.getPodcasts?.podcasts?.map(
-            ({ id, title, category, coverImg, rating }, idx) => (
+          data.getPodcasts?.podcasts
+            ?.filter((podcast) => podcast.category.includes(select))
+            .map(({ id, title, category, coverImg, rating }, idx) => (
               <PodcastItem
                 key={idx}
                 id={id}
@@ -83,8 +113,7 @@ export const Home: React.FC = () => {
                 coverImg={coverImg}
                 rating={rating ? rating : 0}
               />
-            )
-          )}
+            ))}
       </div>
     </>
   );
